c9c934629130a98f5eb88c11b4931604e33d9f68
Paulaasal <paulaarango95@gmail.com>
2024-06-29 18:40:02 +0200

    Convertidor de Celsius a Fahrenheit
    Código sencillo para realizar la conversión de grados Celsius a Fahrenheit

---
+++ Ejercicio1.py
@@ -0,0 +1,17 @@
+# Escribe un programa que convierta una temperatura de grados Celsius a grados Fahrenheit.
+def celsius_top_fahrenheit(celsius):
+  fahrenheit= (celsius*3/5+32)
+  return fahrenheit
+
+fahrenheit = celsius_top_fahrenheit(3)
+print(fahrenheit)
+
+
+
+
+
+
+
+
+
+
