abcc1fe69f7b06f6813072ee8a7fe38bd9f9af6b
Paulaasal <paulaarango95@gmail.com>
2024-06-30 20:02:14 +0200

    Contador de vocales
    programa que cuenta el número de vocales en una palabra ingresada por el usuario.

---
+++ Ejercicio4.py
@@ -0,0 +1,12 @@
+"""Crea un programa que cuente el número de vocales en una palabra ingresada por el usuario."""
+palabra= (input("escribe tu palabra aqui: "))
+vocales = "AEIOUaeiou"
+count = 0
+for letra in palabra:
+  if letra in vocales:
+    count += 1
+print("el numero de vocales en la palabra es: ", count)
+
+
+
+
