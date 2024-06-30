5c05cef56b3baf187569a92af92bcda62ff1bbef
Paulaasal <paulaarango95@gmail.com>
2024-06-30 17:11:04 +0200

    Calculadora de propina
    programa que calcula el monto total a pagar en un restaurante, incluyendo una propina del 15% sobre el total de la cuenta.

---
+++ Ejercicio2.py
@@ -0,0 +1,36 @@
+"""Crea un programa que calcule el monto total a pagar en un restaurante, incluyendo una propina del 15% sobre el total de la cuenta."""
+plato_1= 32
+plato_2= 12
+plato_3= 6
+mi_lista = [plato_1,plato_2,plato_3]
+
+
+def suma_platos(precios_propina):
+  total = 0
+  for precio in precios_propina:
+    total += precio
+    monto_total_propina = total * 1.15
+  return monto_total_propina
+
+print(suma_platos(mi_lista))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
