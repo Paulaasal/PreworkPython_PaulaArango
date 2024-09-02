a0842ead28f7454070d3cce4a5f0791fae2c7f98
Paulaasal <paulaarango95@gmail.com>
2024-09-02 18:21:25 +0200

    Calculadora IMC
    Calculadora para calcular el índice de Masa Corporal. Debe ser usada con Kg y metros.

--- Calculadora_Basica.py
+++ Calculadora_Basica.py
@@ -28,13 +28,13 @@ while True:
     a = float(input("Introduce el primer número: "))
     b = float(input("Introduce el segundo número: "))
 
-    if opcion == '1':
+    if opcion == "1":
         print(f"El resultado de la suma es: {sumar(a, b)}")
-    elif opcion == '2':
+    elif opcion == "2":
         print(f"El resultado de la resta es: {restar(a, b)}")
-    elif opcion == '3':
+    elif opcion == "3":
         print(f"El resultado de la multiplicación es: {multiplicar(a, b)}")
-    elif opcion == '4':
+    elif opcion == "4":
         print(f"El resultado de la división es: {dividir(a, b)}")
     else:
         print("Opción no válida")
