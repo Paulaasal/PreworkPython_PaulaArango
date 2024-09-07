14ee3f2a2be8529f2349c1e1b473ce1c72a34f66
Paulaasal <paulaarango95@gmail.com>
2024-09-07 13:12:28 +0200

    Calculadora Edad
    Programa que solicite al usuario su año de nacimiento y calcule su edad actual.

---
+++ Calculadora_edad.py
@@ -0,0 +1,18 @@
+def valor():
+  año_nacimiento = int(input("introduce tu año de nacimiento"))
+  return año_nacimiento
+
+def años():
+  edad= (2024 - año_nacimiento )
+  return edad
+
+año_nacimiento = valor()
+edad = años()
+  
+print (input(f"actualmente tienes {edad} años"))
+
+  
+  
+
+
+
