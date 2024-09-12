2c796b270ff4a6dc6c0456d7e53c50c026c8db15
Paulaasal <paulaarango95@gmail.com>
2024-09-12 21:29:32 +0200

    Calculadora Descuento
    Programa que determina el precio final de un artículo aplicando el descuento determinado por el usuario.

--- Calculadora_Descuento.py
+++ Calculadora_Descuento.py
@@ -1,3 +1,4 @@
+# calcule el precio final de un artículo después de aplicar un descuento determinado por el usuario
 def precio_articulo():
   precio = float(input("Introduce el precio del artículo "))
   return precio
@@ -6,13 +7,13 @@ def precio_descuento(precio,descuento_porcentaje):
   descuento = precio * (descuento_porcentaje/ 100)
   return (precio- descuento)
 
-def precio_final(precio):
+def precio_final(precio_inicial):
   descuento_porcentaje = float(input("introduce el descuento a aplicar sin el signo de % "))
   return precio_descuento(precio,descuento_porcentaje)
 
 precio = precio_articulo()
 precio_final_calculado= precio_final(precio)
 
-print(f"El precio final aplicando el descuento es: {precio_final_calculado}")
+print(f"El precio final aplicando el descuento es: €{precio_final_calculado}")
 
 
